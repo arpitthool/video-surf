888e21452f42b5b345d8b7e67cbe1506fb09672d
arpitthool <arpitthool1@gmail.com>
2022-07-27 22:24:12 -0400

    initial spring boot project

---
+++ src/main/java/com/arpitthool/app/videosurf/VideoSurfApplication.java
@@ -0,0 +1,13 @@
+package com.arpitthool.app.videosurf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoSurfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoSurfApplication.class, args);
+	}
+
+}
