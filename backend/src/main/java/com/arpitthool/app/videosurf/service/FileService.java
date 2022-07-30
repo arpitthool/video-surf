a026f598d4ca7ca7c3df60da619e601f8f28e847
arpitthool <arpitthool1@gmail.com>
2022-07-29 22:48:59 -0400

    FileService.java: defined uploadFile() schema

--- backend/src/main/java/com/arpitthool/app/videosurf/service/FileService.java
+++ backend/src/main/java/com/arpitthool/app/videosurf/service/FileService.java
@@ -1,4 +1,8 @@
 package com.arpitthool.app.videosurf.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public interface FileService {
+    // uploads file to cloud and return URL
+    public String uploadFile(MultipartFile file);
 }
