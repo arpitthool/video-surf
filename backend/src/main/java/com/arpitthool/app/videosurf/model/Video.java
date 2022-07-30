88285f37c353c479bf00ce006bd1f1c26d4eb562
arpitthool <arpitthool1@gmail.com>
2022-07-29 22:17:55 -0400

    Video.java: make this class mongodb document using @Document annotation

--- backend/src/main/java/com/arpitthool/app/videosurf/model/Video.java
+++ backend/src/main/java/com/arpitthool/app/videosurf/model/Video.java
@@ -1,10 +1,12 @@
 package com.arpitthool.app.videosurf.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 import java.util.Set;
 
+@Document(value = "Video")
 public class Video {
     @Id
     private String id;
