287bc7c389aa219ba12da5d6e137168e60a2e283
arpitthool <arpitthool1@gmail.com>
2022-07-29 22:21:43 -0400

    Video.java: added lombok's @NoArgsConstructor & @AllArgsConstructor which tels lombok to auto generate the corresponding constructors during compilation

--- backend/src/main/java/com/arpitthool/app/videosurf/model/Video.java
+++ backend/src/main/java/com/arpitthool/app/videosurf/model/Video.java
@@ -1,6 +1,8 @@
 package com.arpitthool.app.videosurf.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -9,6 +11,8 @@ import java.util.Set;
 
 @Document(value = "Video")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Video {
     @Id
     private String id;
