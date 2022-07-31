2717fc62c9ac8d05aaa5e0d6f1d14e59dc46085e
arpitthool <arpitthool1@gmail.com>
2022-07-31 19:23:03 -0400

    video.service.ts: calling backend endpoint to upload file with file in FormData object

--- frontend/video-surf-ui/src/app/video.service.ts
+++ frontend/video-surf-ui/src/app/video.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class VideoService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  uploadVideo(file: File) {
+    // create new FormData object to be passed while calling backend
+    const formData = new FormData()
+    // set file parameter
+    formData.append('file', file, file.name)
+    // make a HTTP POST to backend API
+    return this.httpClient.post('http://localhost:8080/api/videos', formData);
+  }
 }
