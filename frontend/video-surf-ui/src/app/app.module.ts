2475dcc6a89192cd0be9a29ca1fa698ebd73a3e3
arpitthool <arpitthool1@gmail.com>
2022-07-31 18:20:43 -0400

    app.module.ts: added necessary imports for upload file component

--- frontend/video-surf-ui/src/app/app.module.ts
+++ frontend/video-surf-ui/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UploadVideoComponent } from './upload-video/upload-video.component';
+import {NgxFileDropModule} from "ngx-file-drop";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { UploadVideoComponent } from './upload-video/upload-video.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    NgxFileDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
