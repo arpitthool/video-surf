25015a89a3af2b70aba2d4f514a2e7ffc52bb205
arpitthool <arpitthool1@gmail.com>
2022-08-05 23:15:12 -0400

    WebMvcConfig.java: TODO: COrS error

---
+++ backend/src/main/java/com/arpitthool/app/videosurf/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.arpitthool.app.videosurf.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+//        corsRegistry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .maxAge(3600)
+//                .allowedHeaders("*")
+//                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS");
+
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
+                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .allowCredentials(true).maxAge(3600);
+    }
+}
