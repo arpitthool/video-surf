7a56d43dc06f1f81295d5c9bac9908e294587414
arpitthool <arpitthool1@gmail.com>
2022-08-08 02:01:47 -0400

    installed angular flex layout using : 'npm i -s @angular/flex-layout @angular/cdk' and added import in main app

--- frontend/video-surf-ui/src/app/app.module.ts
+++ frontend/video-surf-ui/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { HeaderComponent } from './header/header.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import { SaveVideoDetailsComponent } from './save-video-details/save-video-details.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { SaveVideoDetailsComponent } from './save-video-details/save-video-detai
     NgxFileDropModule,
     MatButtonModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
