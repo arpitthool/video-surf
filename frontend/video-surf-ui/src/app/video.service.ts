092a83ccf178920c8235d05d86bdf05885386588
arpitthool <arpitthool1@gmail.com>
2022-08-08 01:00:04 -0400

    specifying UploadVideoResponse as return type

--- frontend/video-surf-ui/src/app/video.service.ts
+++ frontend/video-surf-ui/src/app/video.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import {UploadVideoResponse} from "./upload-video/UploadVideoResponse";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +10,12 @@ export class VideoService {
 
   constructor(private httpClient: HttpClient) { }
 
-  uploadVideo(file: File) {
+  uploadVideo(file: File): Observable<UploadVideoResponse> {
     // create new FormData object to be passed while calling backend
     const formData = new FormData()
     // set file parameter
     formData.append('file', file, file.name)
     // make a HTTP POST to backend API
-    return this.httpClient.post('http://localhost:8080/api/videos', formData);
+    return this.httpClient.post<UploadVideoResponse>('http://localhost:8080/api/videos', formData);
   }
 }
