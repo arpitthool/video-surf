0a8fb57396b7b32001c4b4ceba3c1e3c50ab01c6
arpitthool <arpitthool1@gmail.com>
2022-08-18 01:14:52 -0400

    passing videoUrl to video-player component

--- frontend/video-surf-ui/src/app/video-player/video-player.component.ts
+++ frontend/video-surf-ui/src/app/video-player/video-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-video-player',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-player.component.css']
 })
 export class VideoPlayerComponent implements OnInit {
+
+  @Input()
+  videoUrl! : string | '';
 
   constructor() { }
 
