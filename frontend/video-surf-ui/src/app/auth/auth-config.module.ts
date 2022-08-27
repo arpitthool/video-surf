c4e9553567bcd3d32f7dabf3db60425b131b2ae0
arpitthool <arpitthool1@gmail.com>
2022-08-27 12:18:51 -0400

    using oauth for login and logoff

--- frontend/video-surf-ui/src/app/auth/auth-config.module.ts
+++ frontend/video-surf-ui/src/app/auth/auth-config.module.ts
@@ -5,7 +5,7 @@ import { AuthModule } from 'angular-auth-oidc-client';
 @NgModule({
     imports: [AuthModule.forRoot({
         config: {
-            authority: 'dev-bw6pqg8v.us.auth0.com',
+            authority: 'https://dev-bw6pqg8v.us.auth0.com',
             redirectUrl: window.location.origin,
             clientId: 'H22kSCyclmrYnnj1QxHm57XwAfsnmbnw',
             scope: 'openid profile offline_access',
